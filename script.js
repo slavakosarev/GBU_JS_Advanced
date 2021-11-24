cb3bb3f9628b309699a3ca26dd5dbabb854bec4d
slavakosarev <slava.kosarev55@gmail.com>
2021-11-24 22:33:11 +0300

    added start files

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const goods = [
+    { title: 'Shirt', price: 150 },
+    { title: 'Socks', price: 50 },
+    { title: 'Jacket', price: 350 },
+    { title: 'Shoes', price: 250 },
+];
+
+const $goodsList = document.querySelector('.goods-list');
+  
+const renderGoodsItem = ({ title, price }) => {
+    return `<div class="goods-item"><h3>${title}</h3><p>${price} rub</p></div>`;
+};
+  
+const renderGoodsList = (list = goods) => {
+    let goodsList = list.map(
+            (item) =>  {
+                return renderGoodsItem(item)
+            }
+        ).join('');
+
+    $goodsList.insertAdjacentHTML('beforeend', goodsList);
+}
+  
+renderGoodsList();
+
+
