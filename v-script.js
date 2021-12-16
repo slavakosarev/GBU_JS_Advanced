c098ae509119b399d1a8f1c373b2b48da0510665
slavakosarev <slava.kosarev55@gmail.com>
2021-12-16 19:58:53 +0300

    vue+express/localhost

--- v-script.js
+++ v-script.js
@@ -1,6 +1,116 @@
 'use strict';
 
-const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
+const API_URL = 'http://127.0.0.1:5500/'
+
+Vue.component('good-card', {
+  template: `
+    <div class="good-card" @click="onClick">
+      <h2>{{ data.title }}</h2>
+      <p>$ {{ data.price }}</p>
+    </div>
+  `,
+  props: ['data'],
+  methods: {
+    onClick() {
+      fetch(`${API_URL}addToCart`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/JSON'
+        },
+        body: JSON.stringify(this.data)
+      })
+        .then(() => {
+          this.$emit('add', this.data)
+        })
+    }
+  }
+})
+
+Vue.component('goods-list', {
+  template: `
+    <div class="goods-list">
+      <good-card 
+        v-for="good of list" 
+        v-bind:key="good.id_product"
+        v-bind:data="good"
+        v-on:add="addToCart"
+      ></good-card>
+    </div>
+  `,
+  props: ['list'],
+  methods: {
+     addToCart(good) {
+      this.$emit('add', good)
+    }
+  }
+})
+
+Vue.component('search', {
+  template: `
+    <div class="search">
+      <input type="text" v-model="searchString" class="goods-search" />
+      <button class="search-button" type="button" v-on:click="onClick">Search</button>
+    </div>
+  `,
+  data() {
+    return {
+      searchString: ''
+    }
+  },  
+  methods: {
+    onClick(){
+      this.$emit('search', this.searchString)
+    }
+  }
+})
+
+Vue.component('cart-item', {
+  template: `
+    <div class="good-card">
+      <h2>{{ data.title }}</h2>
+      <p>$ {{ data.price }}</p>
+      <button v-on:click="onClick">Delete</button>
+    </div>
+  `,
+  props: ['data'],
+  methods: {
+    onClick() {
+      fetch(`${API_URL}removeFromCart`, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/JSON'
+        },
+        body: JSON.stringify(this.data)
+      })
+        .then(() => {
+          this.$emit('delete', this.data)
+        })
+    }
+  }
+})
+
+Vue.component('cart', {
+  template: `
+    <div class="modal">
+      <cart-item 
+          v-for="good of list" 
+          v-bind:key="good.id_product"
+          v-bind:data="good"
+          v-on:delete="removeFromCart"
+        ></cart-item>
+        <button v-on:click="onClose">Закрыть</button>
+    </div>
+  `,
+  props: ['list'],
+  methods: {
+    removeFromCart(good) {
+      this.$emit('delete', good)
+    },
+    onClose(){
+      this.$emit('close')
+    }
+  }
+})
 
 new Vue({
    el: '#app',
@@ -8,7 +118,6 @@ new Vue({
       goods: [],
       filteredGoods: [],
       cart: [],
-      searchLine: '',
       isVisibleCart: false
    },
    methods: {
@@ -26,20 +135,18 @@ new Vue({
             .then((request) => request.json())
             .then((data) => this.cart = data.contents)
       },
-      addToCart() {
-         fetch(`${API_URL}addToBasket.json`)
-         .then(() => this.cart.push(good))
+      addToCart(good) {
+         this.cart.push(good)
       },
       removeFromCart(good) {
-         fetch(`${API_URL}deleteFromBasket.json`)
-            .then(() => {
-               const index = this.cart.findIndex((item) => item.id_product == good.id_product);
-               this.cart.splice(index - 1, 1)
-         })
+         const index = this.cart.findIndex((item) => item.id === good.id);
+         if(index >= 0) {
+            this.cart = [...this.cart.slice(0, index), ...this.cart.slice(index + 1)]
+         }
       },
       onSearch() {
          const reg = new RegExp(this.searchLine, 'i');
-         this.filteredGoods = this.goods.filter((good) => reg.test(good.product_name))
+         this.filteredGoods = this.goods.filter((good) => reg.test(good.title))
       },
       onToggleCart() {
          this.isVisibleCart = !this.isVisibleCart
@@ -56,47 +163,3 @@ const regexp = /\B'|'\B/g;
 const $textReg2 = $textReg.replace(regexp, '"');
 document.querySelector('.text').textContent = $textReg2;
 
-const $form = document.querySelector('.form');
-const $name = document.querySelector('#name');
-const $phone = document.querySelector('#phone');
-const $mail = document.querySelector('#mail');
-const $error = document.querySelector('.empty-inputs');
-
-const nameRegexp = /^[A-Za-z]+$/;
-const phoneRegexp = /^\+\d{1}\(\d{3}\)\d{3}-\d{4}$/;
-const mailRegExp = /^[-._a-z0-9]+@(?:[a-z0-9][-a-z0-9]+\.)+[a-z]{2,6}$/;
-
-$form.addEventListener('input', event => {
-    if ($name.value === '' && $phone.value === '' && $mail.value === '') {
-        event.preventDefault();
-        $error.innerText = 'Пожалуйста, заполните поля ввода!';
-    }
-    if ($name.value !== '' && nameRegexp.test($name) === false) {
-        event.preventDefault();
-        $name.style.outline = '3px solid red';
-        $name.nextElementSibling.innerText = 'Имя должно содержать только буквы!'
-    }
-    if ($phone.value !== '' && phoneRegexp.test($phone) === false) {
-        event.preventDefault();
-        $phone.style.outline = '3px solid red';
-        $phone.nextElementSibling.innerText = 'Формат ввода +7(000)000-0000';
-    }
-    if ($mail.value !== '' && mailRegExp.test($mail) === false) {
-        event.preventDefault();
-        $mail.style.outline = '3px solid red';
-        $mail.nextElementSibling.innerText = 'Формат ввода E-mail mymail@mail.ru, или my.mail@mail.ru, или my-mail@mail.ru';
-    }
-});
-function checkInputs() {
-    $form.addEventListener('submit', event => {
-        const arr = [nameRegexp, phoneRegexp, mailRegExp];
-        arr.forEach(elem => {
-            if (event.target.value !== '' && elem.test(event.target) === true) {
-                event.target.style.removeProperty('outline');
-                event.target.nextElementSibling.innerText = '';
-                $error.innerText = '';
-            }
-        });
-    });
-}
-checkInputs($name, $phone, $mail);
